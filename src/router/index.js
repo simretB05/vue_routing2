1d84a6bf523bf2eb0a0cf4b87c30f45568e102d3
simretB05 <happysimret@gmail.com>
2023-04-22 14:13:35 -0600

    added meta tags

--- src/router/index.js
+++ src/router/index.js
@@ -53,8 +53,8 @@ router.beforeEach( ( to, from, next ) =>
   }
     let new_meta_tag = to[`meta`]
     document.querySelector(`title`)[`innerText`] = new_meta_tag[0][`title`]
-    for ( let i = 0; i < new_meta_tag.length; i++ ){
-      document.querySelector( `head` ).insertAdjacentHTML( `beforeend`, '<meta name="${new_meta_tag[i][`name]} content="${new_meta_tag[i][`content`]}">' )
+    for ( let i = 1; i < new_meta_tag.length; i++ ){
+      document.querySelector(`head`).insertAdjacentHTML( `beforeend`, `<meta name="${new_meta_tag[i][`name`]} content="${new_meta_tag[i][`content`]}">` )
     }
       document.querySelector( `head` ).insertAdjacentHTML( `afterbegin`,` <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
